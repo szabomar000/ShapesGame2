6d54c224e7abe12bfa44548fd0b0e759aa65ae95
hiltjar000 <hiltjar000@warren.k12.in.us>
2017-02-16 10:57:42 -0500

    Turret Extension

--- src/Turret.java
+++ src/Turret.java
@@ -4,17 +4,10 @@ import java.awt.*;
  * Created by szabomar000 on 2/15/2017.
  */
 public class Turret extends Entity{
-    private int x, y, width, height;
-    private Game game;
-
 
     public Turret(int x ,int y, int width, int height, Game game){
         super(Color.RED, x, y, width, height, 0,0, game);
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.game = game;
+
 
 
     }
@@ -22,10 +15,10 @@ public class Turret extends Entity{
 
     public void paint(Graphics g){
         g.setColor(Color.red);
-        g.fillRect(x, y, width, height);
+        g.fillRect(getX(), getY(), getWidth(), getHeight());
 
         g.setColor(Color.darkGray);
-        g.fillOval(x+width/4, y+height/4, width/2, height/2);
+        g.fillOval(getX()+getWidth()/4, getY()+getHeight()/4, getWidth()/2, getHeight()/2);
     }
 
     public static Turret makeTurret( Game game){
