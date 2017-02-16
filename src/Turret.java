2e35a5dab54869cb509cb598fcd96a93caa8dc25
hiltjar000 <hiltjar000@warren.k12.in.us>
2017-02-16 11:19:57 -0500

    Drawing

--- src/Turret.java
+++ src/Turret.java
@@ -5,9 +5,10 @@ import java.awt.*;
  */
 public class Turret extends Entity{
 
+    double angle;
     public Turret(int x ,int y, int width, int height, Game game){
         super(Color.RED, x, y, width, height, 0,0, game);
-
+        angle = 0;
 
 
     }
@@ -19,13 +20,25 @@ public class Turret extends Entity{
 
         g.setColor(Color.darkGray);
         g.fillOval(getX()+getWidth()/4, getY()+getHeight()/4, getWidth()/2, getHeight()/2);
-        g.fillRect(getX()+getWidth()/2, getY()+5*getHeight()/12, getWidth()/2, getHeight()/4);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.rotate(angle, getX()+getWidth()/2, getY()+getHeight()/2);
+        g2d.fillRect(getX()+getWidth()/2, getY()+5*getHeight()/12, getWidth()/2, getHeight()/4);
+
+        updateAngle();
 
     }
 
     public static Turret makeTurret( Game game){
         Point point = game.randomPosition();
         return new Turret((int) point.getX(),(int) point.getY(), 30, 30, game);
+    }
+
+    public void updateAngle(){
+        int posX = getGame().getPositionX();
+        int posY = getGame().getPositionY();
+
+        double slope =(double)(posY - getY())/(double)(posX - getX());
+        angle = Math.atan(slope);
     }
 
 
